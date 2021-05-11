7e77963201349f09e05b6136ebb966faf3ade97e
chrockny <chrockny1007@gmail.com>
2021-05-11 13:23:14 -0500

    USEREDUCER

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,17 +1,17 @@
-import React ,{useState,useEffect,useReducer}from 'react';
+import React ,{useState,useEffect,useReducer,useMemo}from 'react';
 import './styles/Character.css';
 
-const initialState ={
+const initialState = {
     favorites:[],
 }
 
-const favoriteReducer = (state, action) =>{
+const favoriteReducer = (state,action) =>{
     switch(action.type){
         case 'ADD_TO_FAVORITE':
             return{
                 ...state,
-                favorites:[...state.favorites,action.payload],
-            };
+                favorites:[...state.favorites, action.payload]
+            }
         default:
             return state;
     }
@@ -20,7 +20,7 @@ const favoriteReducer = (state, action) =>{
 const Characters = () =>{
 
     const [characters,setCharacters] = useState([]);
-    const [favorites, dispatch] = useReducer(favoriteReducer, initialState);
+    const [favorites,dispatch] = useReducer(favoriteReducer,initialState);
 
     useEffect(()=>{
         //Usando fetch para traer los datos
@@ -31,15 +31,20 @@ const Characters = () =>{
     },[])
 
     const handleClick = favorite =>{
-        dispatch({type:'ADD_TO_FAVORITE',payload:favorite})
+            dispatch({type:'ADD_TO_FAVORITE',payload:favorite})
+        
     }
+
+
     return(
         <div className="Characters">
             {favorites.favorites.map(favorite =>(
                 <li key={favorite.id}>
                     {favorite.name}
                 </li>
-            ))}
+            ))
+
+            }
             <h2>{characters.map(character =>(
                 <div className="item" key={character.id}>
                 <h4>{character.name}</h4>
